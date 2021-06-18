1077e02c57f165aa181d1826517e83a57450481d
Ronaldo Rodrigues <guilherme.rsantos@bol.com.br>
2021-06-18 15:44:06 -0300

    mudando

--- ExerciciosMysql/ExerciciosBasicos/03-Select_Escola.sql
+++ ExerciciosMysql/ExerciciosBasicos/03-Select_Escola.sql
@@ -1,16 +1,11 @@
-/*Popule esta tabela com até 8 dados;*/
-
 use db_escola;
 
-insert into tb_estudantes(nome_Estudante, idade_Estudante, rg_Estudante, cpf_Estudante, nota_Estudante)
-	values("Guilherme", 19, "395874159", "58789654789", 8),
-    ("Paulo", 20,  "325874159", "48789654789", 7),
-    ("maria", 19,  "395874159", "58789654789", 9),
-    ("joão", 19,   "395874159", "78956412365", 10),
-    ("Felipe", 19, "257894578", "78965456987", 5),
-    ("Carlos", 19, "789563277", "21457896523", 6),
-    ("Amanda", 19, "789654236", "10254789632", 4),
-    ("Josefa", 19, "202478965", "98745632147", 8),
-    ("Paula", 19,  "123456789", "02487978745", 4);
-    
-select * from tb_estudantes;    
+/*Faça um select que retorne o/as estudantes com a nota maior do que 7.
+Faça um select que retorne o/as estudantes com a nota menor do que 7.
+Ao término atualize um dado desta tabela através de uma query de atualização.*/
+
+select * from tb_Estudantes where nota_Estudante > 7;
+select * from tb_Estudantes where nota_Estudante < 7;
+
+update tb_Estudantes set nome_Estudante = "Paola" where id_Estudante = 9;
+select * from tb_Estudantes;
